3cd1a47a22128cac50beb30db7b01fcb7329d3b3
Jorge <Jorge@DESKTOP-FVTM1P2>
2018-10-02 21:54:53 -0300

    Textos en lang

---
+++ lang/es/local_encuestascdc.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+* Textos de encuestas UAI Corporate
+*
+* @package local
+* @subpackage encuestascdc
+* @copyright 2018-onwards Universidad Adolfo Ibáñez
+* @author Jorge Villalón <jorge.villalon@uai.cl>
+* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+
+$string['pluginname']='UAI Corporate';
+
+// Reporte encuestas CDC
+$string['questionnaire']='Encuesta';
+$string['selectquestionnaire']='Seleccione encuesta';
+$string['questionnaire_help']='Seleccione la encuesta para la cual se generará el reporte';
+$string['questionnaire_report_layout']='Plantilla de reporte';
+
